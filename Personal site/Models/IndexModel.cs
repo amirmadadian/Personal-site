777f3ab21cf995c72cc2b3f7f953b95a477245a1
madadian <madadian@FARAVIN>
2024-05-07 15:21:10 +0430

    solved partial view problem

---
+++ Personal site/Models/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Personal_site.Models
+{
+	public class IndexModel
+	{
+        public List<Project> projects { get; set; }
+        public List<Article> articls  { get; set; }
+    }
+}
---
+++ Personal site/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Permissions;
+
+namespace Personal_site.Models
+{
+	public class Project
+	{
+		public long Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Client { get; set; }
+	}
+}
