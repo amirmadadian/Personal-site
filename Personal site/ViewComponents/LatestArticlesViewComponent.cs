ae9d1ded2379d6a15414488c9a1eb00d8c662279
madadian <madadian@FARAVIN>
2024-05-07 18:17:55 +0430

    articles view components added

---
+++ Personal site/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personal_site.Models;
+using System.Collections.Generic;
+
+namespace Personal_site.ViewComponents
+{
+	public class LatestArticlesViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var articles = new List<Article>
+			{
+				new Article(1,"آموزش BMS","ساده ترین بیان هوشمند سازی","blog_image.jpg"),
+				new Article(2,"ساخت تابلو","ساخت تابلو های کنترلی","electical-panels.jpg"),
+				new Article(3,"پروژه","معرفی پروژه","Tools-16.jpg"),
+			};
+			return View("_LatestArticles",articles);
+		}
+
+	}
+}
