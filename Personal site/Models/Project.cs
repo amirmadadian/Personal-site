654929c790b994f66f25075a8ae9c5d5dfc78deb
madadian <madadian@FARAVIN>
2024-05-07 16:55:00 +0430

    view component added

--- Personal site/Models/Project.cs
+++ Personal site/Models/Project.cs
@@ -4,9 +4,20 @@ namespace Personal_site.Models
 {
 	public class Project
 	{
+		
 		public long Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public string Client { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+
+		public Project(long id, string name, string description,string image, string client)
+		{
+			Id = id;
+			Name = name;
+			Description = description;
+			Image = image;
+			Client = client;
+		}
 	}
 }
---
+++ Personal site/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Personal_site.Models;
+
+namespace Personal_site.ViewComponents
+{
+	public class ProjectsViewComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<Project>
+			{
+				new Project(1,"سپیدار","  مدیریت منابع ومتریال از پروژه های عظیم تا فروشگاه کوچک ","project-5.jpg","Sepidar"),
+				new Project(2,"پارسی برق","  طراحی و ساخت تابلو های کنترلی و قدرت برای پروژه ها و اجرای پروزه ها ","IMG_20230930_113226.jpg","ParsiBragh"),
+				new Project(3,"سپ دات نت","  برنامه نویسی حوزه وب ","project-4.jpg","Sepidar"),
+				new Project(4,"پروژه ها","  خدمات مهندسی و طراحی وپیاده سازی پروژه های تجاری و مسکونی  ","1635690464254.jpeg","ParsiBargh"),
+			};
+			return View("_Projects",projects);
+		}
+	}
+}
